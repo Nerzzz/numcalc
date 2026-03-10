c409c177261de2d8abb82f1846b21a6116e4c21f
Nerzzz <guilhermeschiavolnsantos@gmail.com>
2026-03-10 11:26:04 -0300

    fix: melhorias no tratamento de entrada

--- script.js
+++ script.js
@@ -3,14 +3,30 @@ function calculate(){
     const fromBase = parseInt(document.getElementById("inBase").value);
     const toBase = parseInt(document.getElementById("outBase").value);
 
-    decimal = parseInt(value, fromBase);
-
-    if(isNaN(decimal)){
-        document.getElementById("outValue").innerHTML = "Preencha corretamente!";
+    if(value == ""){
+        document.getElementById("outValue").innerHTML = "Esperando entrada...";
+        return;
+    }
+    
+    if(fromBase == 2 && !/[0-1]/.test(value)){
+        document.getElementById("outValue").innerHTML = "Apenas 0 e 1 aceitos";
+        return;
+    }
+    if(fromBase == 8 || fromBase == 10 && /[A-Za-z]/.test(value)){
+        document.getElementById("outValue").innerHTML = "Não utilize letras para essa base";
+        return;
+    }
+    if(fromBase == 16 && /[G-Zg-z]/.test(value)){
+        document.getElementById("outValue").innerHTML = "Caracteres inválidos";
         return;
     }
 
-    if(fromBase == 2 && !/[0-1]/.test(value)) return;
+    decimal = parseInt(value, fromBase);
+
+    if(isNaN(decimal)){
+        document.getElementById("outValue").innerHTML = "Preencha corretamente";
+        return;
+    }
 
     document.getElementById("outValue").innerHTML = decimal.toString(toBase).toUpperCase();
 }
